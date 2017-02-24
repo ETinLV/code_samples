28d96bda7d7b01296e6881ef6e178f1ab890a5a1
Eric Turner <eturner@gamingarts.com>
2017-02-23 16:17:12 -0800

    refactor

--- hockey.py
+++ hockey.py
@@ -1,32 +1,37 @@
+import datetime
 import json
-from datetime import datetime
 from pprint import pprint
 
 import requests
 
-STAT_URL = 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=2017-02-09&endDate=2017-02-14&expand=schedule.teams,schedule.linescore,schedule.broadcasts.all,schedule.ticket,schedule.game.content.media.epg,schedule.radioBroadcasts,schedule.decisions,schedule.scoringplays,schedule.game.content.highlights.scoreboard,team.leaders,schedule.game.seriesSummary,seriesSummary.series&leaderCategories=points,goals,assists&leaderGameTypes=R&site=en_nhl&teamId=&gameType=&timecode='
+STAT_URL = 'https://statsapi.web.nhl.com/api/v1/schedule'
+
 
 def make_request():
     """Make the request to NHL website"""
-    return requests.get(STAT_URL)
+    today = datetime.datetime.today()
+    one_week_ago = (today - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+    today = today.strftime('%Y-%m-%d')
+    params = {'startDate': one_week_ago,
+              'endDate': today,
+              'expand': 'schedule.teams,schedule.linescore,'}
+    return requests.get(STAT_URL, params=params).content
 
 
-def parse_response():
+def parse_response(content):
     """Convert response to JSON Dict"""
-    return json.loads(make_request().content)
+    return json.loads(content)
 
 
 def scores_for_today():
-    """Get score's object for today"""
-    today = datetime.today()
-    daily_scores_json = parse_response()
+    """Get score's object for most recent day with games"""
+    content = make_request()
+    daily_scores_json = parse_response(content)
     todays_games = {}
-    for date in daily_scores_json['dates']:
-        if datetime.strptime(date['date'], '%Y-%m-%d').date() == today.date():
+    for date in daily_scores_json['dates'][::-1]:
+        if date['games']:
             todays_games = date['games']
             break
-        else:
-            continue
     return todays_games
 
 
@@ -35,43 +40,32 @@ def make_scores_object():
     scores = []
     all_scores = scores_for_today()
     for game in all_scores:
-        d = {'venue': game['venue']['name'],
-             'away': {
-                 'name': game['teams']['away']['team']['name'],
-                 'short_name': game['teams']['away']['team']['shortName'],
-                 'abbreviation': game['teams']['away']['team']['abbreviation'],
-                 'score': game['teams']['away']['score']
-             },
-             'home': {
-                 'name': game['teams']['home']['team']['name'],
-                 'short_name': game['teams']['home']['team']['shortName'],
-                 'abbreviation': game['teams']['home']['team']['abbreviation'],
-                 'score': game['teams']['home']['score']
-             },
+        d = {'away': make_team_obect(game['teams']['away']),
+             'home': make_team_obect(game['teams']['home']),
+             'venue': game['venue']['name'],
              'current_period': game['linescore']['currentPeriod'],
              'current_period_ordinal': game['linescore'].get('currentPeriodOrdinal', 'Scheduled'),
              'current_period_time_remaining': game['linescore'].get('currentPeriodTimeRemaining', "N/A")
-             }
-        scores.append(dict_to_utf_8(d))
+        }
+        scores.append(d)
     return scores
+
+
+def make_team_obect(team):
+    team_info = {'name': team['team']['name'],
+                 'short_name': team['team']['shortName'],
+                 'abbreviation': team['team']['abbreviation'],
+                 'score': team['score']
+                 }
+    return team_info
 
 
 def print_scores():
     scores = make_scores_object()
     if len(scores) == 0:
         scores = 'No Games Today'
-    pprint(scores)
+    print(json.dumps(scores, indent=4))
 
-
-def dict_to_utf_8(d):
-    """Convert a dict with unicode to utf-8, this removes the u'' from printed text"""
-    for k,v in d.iteritems():
-        if isinstance(v, dict):
-            dict_to_utf_8(v)
-        new_k = k.encode('utf-8', 'ignore') if isinstance(k, unicode) else k
-        d[new_k] = d.pop(k)
-        d[k] = v.encode('utf-8', 'ignore') if isinstance(v, unicode) else v
-    return d
 
 if __name__ == '__main__':
     print_scores()
